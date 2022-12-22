a8167c01ae04b52321ff4de317b1aae0a79e4a91
soyost <121071479+soyost@users.noreply.github.com>
2022-12-22 16:10:23 -0600

    reset main from branch 4_02

--- main.tf
+++ main.tf
@@ -31,25 +31,25 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.7.0"
-  
-  name = "blog"
-  min_size = 1
-  max_size = 2
 
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.0"
+  version = "~> 6.0"
 
   name = "blog-alb"
 
@@ -77,7 +77,7 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "Dev"
+    Environment = "dev"
   }
 }
 
