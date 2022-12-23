d33cc9116e5c7598d9541ebcf553b9e2a896d62b
soyost <121071479+soyost@users.noreply.github.com>
2022-12-22 19:55:15 -0600

    dfvff

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "blog_autoscaling" {
   name = "blog"
 
   min_size            = 3
-  max_size            = 2
+  max_size            = 4
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
